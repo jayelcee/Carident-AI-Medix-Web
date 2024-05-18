e88d685e5c28e473ad9c84229fc7e309ad5f337f
jayelcee <camasurajasmine01@gmail.com>
2024-05-18 23:14:24 +0800

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,145 @@
+import carident from "./carident.svg";
+import check from "./check.svg";
+import caridentSymbol from "./carident-symbol.svg";
+import brainwaveWhiteSymbol from "./brainwave-symbol-white.svg";
+import play from "./play.svg";
+import gradient from "./gradient.png";
+import smallSphere from "./4-small.png";
+import grid from "./grid.png";
+import check2 from "./check-02.svg";
+import loading1 from "./loading-01.svg";
+import yourlogo from "./yourlogo.svg";
+import home from "./home.svg";
+import plusSquare from "./plus-square.svg";
+import maps from "./maps.svg";
+import recording03 from "./recording-03.svg";
+import recording01 from "./recording-01.svg";
+import disc02 from "./disc-02.svg";
+import chromecast from "./chrome-cast.svg";
+import sliders04 from "./sliders-04.svg";
+import loading from "./loading.png";
+import background from "./background.jpg";
+
+import curve from "./hero/curve.png";
+import analyze from "./hero/analyze.png";
+import heroBackground from "./hero/hero-background.jpg";
+
+import curve1 from "./collaboration/curve-1.svg";
+import curve2 from "./collaboration/curve-2.svg";
+import discord from "./collaboration/discord.png";
+import figma from "./collaboration/figma.png";
+import framer from "./collaboration/framer.png";
+import notion from "./collaboration/notion.png";
+import photoshop from "./collaboration/photoshop.png";
+import protopie from "./collaboration/protopie.png";
+import raindrop from "./collaboration/raindrop.png";
+import slack from "./collaboration/slack.png";
+
+import service1 from "./services/service-1.png";
+import service2 from "./services/service-2.png";
+import service3 from "./services/service-3.png";
+
+import lines from "./pricing/lines.svg";
+import stars from "./pricing/stars.svg";
+
+import coins from "./roadmap/coins.png";
+import done from "./roadmap/done.svg";
+import hero from "./roadmap/hero.png";
+import roadmap1 from "./roadmap/image-1.png";
+import roadmap2 from "./roadmap/image-2.png";
+import roadmap3 from "./roadmap/image-3.png";
+import roadmap4 from "./roadmap/image-4.png";
+import undone from "./roadmap/undone.svg";
+
+import notification1 from "./notification/image-1.jpg";
+import notification2 from "./notification/image-2.jpg";
+import notification3 from "./notification/image-3.jpg";
+import notification4 from "./notification/image-4.jpg";
+
+import featuresCard1 from "./features/card-1.svg";
+import featuresCard2 from "./features/card-2.svg";
+import featuresCard3 from "./features/card-3.svg";
+import featuresCard4 from "./features/card-4.svg";
+import featuresCard5 from "./features/card-5.svg";
+import featuresCard6 from "./features/card-6.svg";
+import featuresIcon1 from "./features/icon-1.svg";
+import featuresIcon2 from "./features/icon-2.svg";
+import featuresIcon3 from "./features/icon-3.svg";
+import featuresIcon4 from "./features/icon-4.svg";
+import featuresImage2 from "./features/image-2.png";
+
+import discordBlack from "./socials/discord.svg";
+import facebook from "./socials/facebook.svg";
+import instagram from "./socials/instagram.svg";
+import telegram from "./socials/telegram.svg";
+import twitter from "./socials/twitter.svg";
+
+export {
+  carident,
+  check,
+  check2,
+  loading1,
+  caridentSymbol,
+  brainwaveWhiteSymbol,
+  play,
+  gradient,
+  smallSphere,
+  grid,
+  yourlogo,
+  home,
+  plusSquare,
+  maps,
+  recording03,
+  recording01,
+  disc02,
+  chromecast,
+  sliders04,
+  loading,
+  background,
+  curve,
+  analyze,
+  heroBackground,
+  curve1,
+  curve2,
+  discord,
+  figma,
+  framer,
+  notion,
+  photoshop,
+  protopie,
+  raindrop,
+  slack,
+  service1,
+  service2,
+  service3,
+  lines,
+  stars,
+  coins,
+  done,
+  hero,
+  roadmap1,
+  roadmap2,
+  roadmap3,
+  roadmap4,
+  undone,
+  notification1,
+  notification2,
+  notification3,
+  notification4,
+  featuresCard1,
+  featuresCard2,
+  featuresCard3,
+  featuresCard4,
+  featuresCard5,
+  featuresCard6,
+  featuresIcon1,
+  featuresIcon2,
+  featuresIcon3,
+  featuresIcon4,
+  featuresImage2,
+  discordBlack,
+  facebook,
+  instagram,
+  telegram,
+  twitter,
+};
